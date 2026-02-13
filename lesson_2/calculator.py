a6ec678c72cc79662285b8c9542785ccb46d89bd
Hermes Manrique <tvloco1@hotmail.com>
2026-02-13 16:55:36 -0500

    refactor: calculator.py

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,29 +1,45 @@
-# Ask the user for the first number.
-# Ask the user for the second number.
-# Ask the user for an operation to perform.
-# Perform the operation on the two numbers.
-# Print the result to the terminal.
+def prompt(message):
+    print(f"==> {message}")
 
-print('Welcome to Calculator!')
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
 
-print('What is the first number?')
-number1 = int(input())
-print('What is the second number?')
-number2 = int(input())
+    return False
 
-print('What operation would you like to perform?\n'
-      '1) Add 2) Substract 3) Multiply 4) Divide')
+prompt('Welcome to Calculator!')
+
+prompt("What's the first number?")
+number1 = input()
+
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
+prompt("What's the second number?")
+number2 = input()
+
+while invalid_number(number2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number2 = input()
+
+prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide")
 operation = input()
-result = 0
+
+while operation not in ["1", "2", "3", "4"]:
+    prompt("You must choose 1, 2, 3, or 4")
+    operation = input()
 
 match operation:
-    case '1':
-        result = number1 + number2
-    case '2':
-        result = number1 - number2
-    case '3':
-        result = number1 * number2
-    case '4':
-        result = number1 / number2
+    case "1":
+        output = int(number1) + int(number2)
+    case "2":
+        output = int(number1) - int(number2)
+    case "3":
+        output = int(number1) * int(number2)
+    case "4":
+        output = int(number1) / int(number2)
 
-print(f'The result is:\n{result}')
+prompt(f"The result is {output}")
