bfab3930cd2c630b6369a35ea112d6b4cfc6fbde
Hermes Manrique <tvloco1@hotmail.com>
2026-02-15 19:29:41 -0500

    refactor: added rerun functionality to calculator.py, added new dictionary file with calculator messages

---
+++ lesson_2/calculator_messages.py
@@ -0,0 +1,9 @@
+messages_dict = {
+    'number1': "What's the first number?",
+    'number2': "What's the second number?",
+    'invalid_numb': "Hmm... that doesn't look like a valid number.",
+    'operation': """What operation would you like to perform?
+    1) Add 2) Subtract 3) Multiply 4) Divide""",
+    'chose': "You must choose 1, 2, 3, or 4",
+    'new_calc': 'Would you like to perform a new calculation? (y/n) ',
+}
